24cd1303c5288172f08c922fa0aaaa20d71950c9
aquaero7 <aquaero7@gmail.com>
2023-11-07 19:53:43 +0100

    TopBar menu and Composable parameters refactoring, in progress

--- app/src/main/java/com/aquaero/realestatemanager/RealEstateManagerActivity.kt
+++ app/src/main/java/com/aquaero/realestatemanager/RealEstateManagerActivity.kt
@@ -2,10 +2,14 @@ package com.aquaero.realestatemanager
 
 import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
@@ -15,6 +19,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.aquaero.realestatemanager.ui.components.app.AppTabRow
@@ -51,17 +56,55 @@ fun RealEstateManagerApp(
             else -> { AppContentType.SCREEN_ONLY }
         }
 
-        // Init data
+        /** Init data */
         val context = LocalContext.current
+
+        /**
+         * Navigation
+         */
         val tabRowScreens = tabRowScreens
         val navController = rememberNavController()
         // Fetch current destination
         val currentBackStack by navController.currentBackStackEntryAsState()
         val currentDestination = currentBackStack?.destination
         val currentScreen = currentDestination?.route
-
         // Use 'ListAndDetail' as a backup screen if the returned value is null
         val currentTabScreen = tabRowScreens.find { it.route == currentScreen } ?: ListAndDetail
+        // Init the click on topBar menu icon
+
+        /**
+         * TopBar
+         */
+        val onClickMenu = {
+            val msg = when (currentScreen) {
+                ListAndDetail.routeWithArgs -> R.string.edit
+                Detail.routeWithArgs -> R.string.edit
+                else -> R.string.valid
+            }
+            Toast.makeText(context, "Click on ${context.getString(msg)}", Toast.LENGTH_SHORT).show()
+        }
+
+        val menuIcon = if (
+            currentScreen == EditDetail.routeWithArgs ||
+            currentScreen == SearchCriteria.route ||
+            currentScreen == Loan.route
+            ) Icons.Default.Check else Icons.Default.Edit
+
+        val menuIconContentDesc = if (
+            currentScreen == EditDetail.routeWithArgs ||
+            currentScreen == Loan.route
+            ) stringResource(id = R.string.cd_check) else stringResource(id = R.string.cd_edit)
+
+        val menuEnabled = (
+                currentScreen != GeolocMap.route) &&
+                (currentScreen != ListAndDetail.routeWithArgs ||
+                contentType == AppContentType.SCREEN_WITH_DETAIL)
+
+        val onClickRadioButton = { currency: String -> Toast
+            .makeText(context, "Click on $currency", Toast.LENGTH_SHORT)
+            .show()
+        }
+
 
         /**
          * Navigate single top to tabs
@@ -70,12 +113,18 @@ fun RealEstateManagerApp(
             navController.navigateSingleTopTo(newScreen)
         }
 
+        /**
+         * Composable
+         */
+
         Scaffold (
             topBar = {
                 AppTopBar(
-                    context = context,
-                    currentScreen = currentScreen,
-                    contentType = contentType
+                    menuIcon = menuIcon,
+                    menuIconContentDesc = menuIconContentDesc,
+                    menuEnabled = menuEnabled,
+                    onClickMenu = onClickMenu,
+                    onClickRadioButton = onClickRadioButton,
                 )
             },
             bottomBar = {
--- app/src/main/java/com/aquaero/realestatemanager/ui/components/app/AppTopBar.kt
+++ app/src/main/java/com/aquaero/realestatemanager/ui/components/app/AppTopBar.kt
@@ -8,12 +8,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AddHome
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.filled.Upload
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -22,6 +18,7 @@ import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -36,98 +33,67 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.aquaero.realestatemanager.Detail
+import com.aquaero.realestatemanager.EditDetail
+import com.aquaero.realestatemanager.GeolocMap
 import com.aquaero.realestatemanager.ListAndDetail
+import com.aquaero.realestatemanager.Loan
 import com.aquaero.realestatemanager.R
+import com.aquaero.realestatemanager.SearchCriteria
 import com.aquaero.realestatemanager.utils.AppContentType
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppTopBar(
     modifier: Modifier = Modifier,
-    context: Context,
-    currentScreen: String?,
-    contentType: AppContentType
+    menuIcon: ImageVector,
+    menuIconContentDesc: String,
+    menuEnabled: Boolean,
+    onClickMenu: () -> Unit,
+    onClickRadioButton: (String) -> Unit,
+    titleText: String = stringResource(id = R.string.app_name),
+    colors: TopAppBarColors = TopAppBarDefaults
+        .topAppBarColors(containerColor = MaterialTheme.colorScheme.primary),
+    dollar: String = stringResource(id = R.string.dollar),
+    euro: String = stringResource(id = R.string.euro),
 ) {
-    val dollar = stringResource(id = R.string.dollar)
-    val euro = stringResource(id = R.string.euro)
     var selectedOption by remember { mutableStateOf(dollar) }
-    var expanded by remember { mutableStateOf(false) }
-    val itemEnabled = (currentScreen == Detail.routeWithArgs)
-            || (currentScreen == ListAndDetail.routeWithArgs
-            && contentType == AppContentType.SCREEN_WITH_DETAIL)
 
     TopAppBar(
         modifier = modifier.height(48.dp),
         title = { Text(
-            text = stringResource(id = R.string.app_name),
+            text = titleText,
             color = Color.White,
             fontSize = 24.sp,
             modifier = Modifier.padding(top = 4.dp)
         ) },
-        colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = MaterialTheme.colorScheme.primary,
-            // containerColor = MaterialTheme.colorScheme.primaryContainer,
-            // containerColor = Color.Cyan,
-        ),
+        colors = colors,
         actions = {
             // RadioButton €
             TopBarRadioButton(
                 selected = selectedOption == euro,
-                onClick = { selectedOption = euro },
+                onClick = { selectedOption = euro; run { onClickRadioButton(euro) } },
                 text = euro,
             )
             // RadioButton $
             TopBarRadioButton(
                 selected = selectedOption == dollar,
-                onClick = { selectedOption = dollar },
+                onClick = { selectedOption = dollar; run { onClickRadioButton(dollar) } },
                 text = dollar,
             )
 
-            // Dropdown menu
+            // Menu
             Box(modifier = Modifier
                 .width(64.dp)
                 .wrapContentSize(Alignment.TopEnd)
             ) {
-                IconButton(onClick = { expanded = !expanded }) {
-                    Icon(
-                        imageVector = Icons.Default.MoreVert,
-                        contentDescription = stringResource(id = R.string.cd_more),
-                        tint = Color.White
-                    )
-                }
-                DropdownMenu(
-                    expanded = expanded,
-                    onDismissRequest = { expanded = false }
+                IconButton(
+                    onClick = onClickMenu,
+                    enabled = menuEnabled,
                 ) {
-                    TopBarDropDownMenuItem(
-                        imageVector = Icons.Default.Upload,
-                        contentDesc = stringResource(id = R.string.cd_export),
-                        text = { Text(stringResource(id = R.string.export)) },
-                        onClick = {
-                            expanded = false
-                            Toast.makeText(context, "Click on Export", Toast.LENGTH_SHORT).show()
-                        },
-                        enabled = true,
-                    )
-                    TopBarDropDownMenuItem(
-                        imageVector = Icons.Default.AddHome,
-                        contentDesc = stringResource(id = R.string.cd_create),
-                        text = { Text(stringResource(id = R.string.create)) },
-                        onClick = {
-                            expanded = false
-                            Toast.makeText(context, "Click on Create", Toast.LENGTH_SHORT).show()
-                        },
-                        enabled = true,
-                    )
-                    TopBarDropDownMenuItem(
-                        imageVector = Icons.Default.Edit,
-                        contentDesc = stringResource(id = R.string.cd_edit),
-                        text = { Text(stringResource(id = R.string.edit)) },
-                        onClick = {
-                            expanded = false
-                            Toast.makeText(context, "Click on Edit", Toast.LENGTH_SHORT).show()
-                        },
-                        enabled = itemEnabled,
+                    Icon(
+                        imageVector = menuIcon,
+                        contentDescription = menuIconContentDesc,
+                        tint = Color.White,
                     )
                 }
             }
@@ -163,28 +129,4 @@ fun TopBarRadioButton(
             modifier = Modifier.padding(start = 36.dp)
         )
     }
-}
-
-@Composable
-fun TopBarDropDownMenuItem(
-    modifier: Modifier = Modifier,
-    imageVector: ImageVector,
-    contentDesc: String,
-    text: @Composable () -> Unit = {},
-    onClick: () -> Unit = {},
-    enabled: Boolean
-
-) {
-    DropdownMenuItem(
-        modifier = modifier,
-        leadingIcon = {
-            Icon(
-                imageVector = imageVector,
-                contentDescription = contentDesc,
-            )
-        },
-        text = text,
-        onClick = onClick,
-        enabled = enabled,
-    )
 }
